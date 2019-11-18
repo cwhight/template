52a0d09140284563d015401f2a549fad7ec6091c
James Middlemass <james.middlemass01@gmail.com>
2019-11-18 13:25:21 +0000

    changed content to text

--- db/migrate/20191118132014_create_reviews.rb
+++ db/migrate/20191118132014_create_reviews.rb
@@ -2,7 +2,7 @@ class CreateReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
       t.string :title
-      t.string :content
+      t.text :content
       t.integer :score
       t.references :user, foreign_key: true
       t.references :shift, foreign_key: true
