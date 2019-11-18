c337994348d98ab5f865d7ff70b89a5a082a2b9b
James Middlemass <james.middlemass01@gmail.com>
2019-11-18 14:33:06 +0000

    reviews controller

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,38 @@
+class ReviewsController < ApplicationController
+  before_action :set_review, only: [:show, :destroy]
+
+  def index
+    @reviews = Review.all
+  end
+
+  def show
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to reviews_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @review.destroy
+    redirect_to reviews_path
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:title, :content, :score)
+  end
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
+end
