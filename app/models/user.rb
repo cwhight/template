53b9455ee83edf19b3d763d1da401151769beac5
Charlie Whight <cdwhight@gmail.com>
2019-11-18 13:30:11 +0000

    add associations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :jobs
+  has_many :reviews
+  has_many :shifts
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
