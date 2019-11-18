8ff0fd4d82d27d0f8ebbc9210f9fa8f7a2d7943e
Chris Williams <chriswilliams14.williams@gmail.com>
2019-11-18 14:14:20 +0000

    Finished Jobs Controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,5 @@
 class JobsController < ApplicationController
-  before_action :set_job, only: [:show]
+  before_action :set_job, only: [:show, :edit, :update, :destroy]
 
   def index
     @jobs = Job.all
@@ -8,7 +8,37 @@ class JobsController < ApplicationController
   def show
   end
 
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to new_job_dose_path(@job)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @job.update(job_params)
+    redirect_to jobs_path
+  end
+
+  def destroy
+    @job.destroy
+    redirect_to jobs_path
+  end
+
   private
+
+  def job_params
+    params.require(:job).permit(:name)
+  end
 
   def set_job
     @job = Job.find(params[:id])
