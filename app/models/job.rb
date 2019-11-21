ef3df8ff27f29c63d3a27773def43a832c48e386
James Middlemass <james.middlemass01@gmail.com>
2019-11-21 13:05:22 +0000

    some validations

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,6 +2,9 @@ class Job < ApplicationRecord
   belongs_to :user
   has_many :shifts
   has_many :reviews, through: :shifts
+  validates :title, presence: true, length: { in: 2..30 }
+  validates :description, presence: true, length: { maximum: 200 }
+  validates :location, presence: true
   include PgSearch::Model
   pg_search_scope :kinda_matching,
                   against: :title,
--- app/models/shift.rb
+++ app/models/shift.rb
@@ -3,4 +3,5 @@ class Shift < ApplicationRecord
   belongs_to :job
   has_many :reviews
   has_many :requests
+  validates :pay, presence: true, numericality: true
 end
