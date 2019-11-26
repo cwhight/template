132b01c0d99aaa56dae344a8b5d12116271eac40
Charlie Whight <cdwhight@gmail.com>
2019-11-26 12:42:30 +0000

    dashboard improvements

--- app/javascript/components/dashboard.js
+++ app/javascript/components/dashboard.js
@@ -1,13 +1,27 @@
-const cards = document.querySelectorAll('.dashboard-card');
+const selectors = document.querySelectorAll('.dashboard-selector');
+const profilecards = document.querySelectorAll('.dashboard-card')
 
-cards.forEach((card) => {
-  card.addEventListener('click', (e) => {
-    const profileCards = document.querySelectorAll('.profile-card')
-    profileCards.forEach((card) => {
+selectors.forEach((selector) => {
+  selector.addEventListener('click', (e) => {
+    profilecards.forEach((card) => {
       card.classList.add('d-none')
       card.classList.remove('text-secondary')
-  });
-    document.getElementById(card.dataset.selector).classList.toggle('d-none')
-    document.getElementById(card.dataset.selector).classList.add('text-secondary')
+      selectors.forEach((selector) => {
+        selector.classList.remove('active')
+      });
+      e.currentTarget.classList.add('active')
+    });
+    document.getElementById(selector.dataset.selector).classList.toggle('d-none')
+    document.getElementById(selector.dataset.selector).classList.add('text-secondary')
   });
 });
+
+const sidebar = document.querySelector("#sidebar")
+const sidebarCollapse = document.getElementById("sidebarCollapse")
+
+sidebarCollapse.addEventListener('click', (e) => {
+  sidebar.classList.toggle('active')
+
+});
+
+
