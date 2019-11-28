2f193a70d18b6cd0a5af0ef70c68e264921ee1c5
Charlie Whight <cdwhight@gmail.com>
2019-11-28 16:11:41 +0000

    create message with request

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -7,11 +7,6 @@ class RequestsController < ApplicationController
   end
 
   def new
-    @request = Request.new
-    authorize @request
-  end
-
-  def confirmation
     @shift = Shift.find(params[:shift_id])
     @job = Job.find(params[:job_id])
 
@@ -25,14 +20,40 @@ class RequestsController < ApplicationController
     authorize @request
   end
 
+  # def confirmation
+  #   @shift = Shift.find(params[:shift_id])
+  #   @job = Job.find(params[:job_id])
+
+  #   @request = Request.new
+
+  #   @markers = [{
+  #     lat: @job.latitude,
+  #     lng: @job.longitude
+  #   }]
+
+  #   authorize @request
+  # end
+
   def create
     @request = Request.new(request_params)
     authorize @request
     @request.user = current_user
     @request.shift = Shift.find(params[:shift_id])
     if @request.save
-      sleep 3
-      redirect_to dashboard_path(current_user)
+      @chat = Chat.new
+      @chat.request = @request
+      @chat.employee = @request.user
+      @chat.employer = @request.shift.job.user
+      if @chat.save
+        @message = Message.new
+        @message.chat = @chat
+        @message.content = @request.content
+        @message.user = @request.user
+        if @message.save
+          sleep 3
+          redirect_to dashboard_path(current_user)
+        end
+      end
     else
       render :new
     end
