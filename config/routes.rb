c7529f48739deb866b8686955f75060d63bbe3d5
James <54590378+jamiddle@users.noreply.github.com>
2019-11-28 17:51:58 +0000

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :jobs do
-    resources :shifts, except: :shows do
+    resources :shifts, except: :show do
       resources :orders, only: [:create]
       resources :requests, except: [:show]
       get 'requests/confirmation', to: 'requests#confirmation'
