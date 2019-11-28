8766115af63609e7a881f6fa09788a098701d675
Chris Williams <chriswilliams14.williams@gmail.com>
2019-11-28 17:47:28 +0000

    stripe3

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :jobs do
-    resources :shifts, except: :show do
+    resources :shifts, except: :shows do
       resources :orders, only: [:create]
       resources :requests, except: [:show]
       get 'requests/confirmation', to: 'requests#confirmation'
@@ -44,5 +44,6 @@ Rails.application.routes.draw do
     mount Sidekiq::Web => '/sidekiq'
   end
 
+  mount StripeEvent::Engine, at: '/stripe-webhooks'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
