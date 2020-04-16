63d69afacde01870dc304db1976fe430858e32b2
cwhight <cdwhight@gmail.com>
2020-04-16 15:24:32 +0100

    chat inbox and notifications

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,9 +4,6 @@ class PagesController < ApplicationController
   def sign_up
   end
 
-  def landing
-  end
-
   def home
     @jobs = policy_scope(Job).order(created_at: :desc)
     @request = Request.new
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   get 'stripe/oauth', to: 'users#stripe_sign_up'
   devise_for :users
   root to: 'pages#home'
-  get '/home', to: 'pages#home', as: :home
+
+# JOBS
+
   resources :jobs do
     resources :favourites, only: [:create, :destroy]
     resources :shifts, except: :show do
@@ -14,57 +16,70 @@ Rails.application.routes.draw do
       get 'requests/confirmation', to: 'requests#confirmation'
     end
   end
+  get 'jobs/:id/close', to: 'jobs#close', as: :job_close
+
+# Payments
+
   resources :orders, only: [:show] do
     resources :payments, only: :new
   end
+  get '/confirmation_page', to: 'pages#confirmation_page'
+
+# Shifts
 
   patch '/shifts/close', to: 'shifts#close_shifts', as: :close_shifts
-
-  get 'jobs/:id/close', to: 'jobs#close', as: :job_close
-  get 'dashboard_employer/closed_jobs', to: 'jobs#closed_jobs', as: :closed_jobs
-  get 'dashboard_employer/listed_jobs', to: 'jobs#listed_jobs', as: :listed_jobs
-
-
-  resources :shifts, only: :show
-
-  get 'users/:id/offers/select', to: 'offers#select', as: :new_user_offer
   get 'shifts/:id/users/:user_id/offers/new', to: 'offers#new', as: :new_shift_offer
   post 'shifts/:id/users/:user_id/offers', to: 'offers#create', as: :create_shift_offer
+  post 'shifts/:shift_id/reviews', to: 'reviews#create', as: :shift_reviews
+  resources :shifts, only: :show
+
+# Offers
+
+  resources :offers, only: :show
+  patch 'offers/:id/accept_offer', to: 'shifts#accept_offer', as: 'accept_offer'
+
+# Requests
+
+  resources :requests, only: [:show] do
+    resources :chats, only: [:create]
+  end
+  patch 'requests/:id/accept_request', to: 'shifts#accept_request', as: 'accept_request'
+
+# Applications
+
+  get '/active_applications', to: 'requests#active_applications', as: :active_applications
+
+# Users
+
+  get 'users/:id/offers/select', to: 'offers#select', as: :new_user_offer
+  resources :users, only: [:edit, :update]
+
+  get '/employees', to: 'users#index', as: :users
+
+# Reviews
 
   get '/reviews', to: 'reviews#review_dashboard', as: :review_dashboard
 
-  post 'shifts/:shift_id/reviews', to: 'reviews#create', as: :shift_reviews
+# Dashboard
+
+  get 'dashboard_employer/closed_jobs', to: 'jobs#closed_jobs', as: :closed_jobs
+  get 'dashboard_employer/listed_jobs', to: 'jobs#listed_jobs', as: :listed_jobs
+  get '/dashboard_employer', to: 'pages#dashboard_employer'
 
   get '/dashboard', to: 'pages#dashboard'
   get 'dashboard/pending', to: 'requests#pending_requests', as: 'pending_requests'
   get 'dashboard/upcoming', to: 'shifts#upcoming_shifts', as: 'upcoming_shifts'
   get 'dashboard/historic', to: 'shifts#historic_shifts', as: 'historic_shifts'
   get 'dashboard/favourites', to: 'favourites#index', as: 'favourites'
-  get '/dashboard_employer', to: 'pages#dashboard_employer'
-
   get '/dashboard/chats/:id', to: 'chats#inbox_show', as: :dashboard_chat
 
-  get '/confirmation_page', to: 'pages#confirmation_page'
 
-  get '/inbox', to: 'chats#inbox', as: :inbox
-
-  get '/active_applications', to: 'requests#active_applications', as: :active_applications
+# Chat
 
   resources :chats, except: :create do
     resources :messages
   end
-
-  get '/employees', to: 'users#index', as: :users
-
-  resources :users, only: [:edit, :update]
-
-  resources :offers, only: :show
-
-  resources :requests, only: [:show] do
-    resources :chats, only: [:create]
-  end
-  patch 'requests/:id/accept_request', to: 'shifts#accept_request', as: 'accept_request'
-  patch 'offers/:id/accept_offer', to: 'shifts#accept_offer', as: 'accept_offer'
+  get '/inbox', to: 'chats#inbox', as: :inbox
 
 
   # authenticate :user, lambda { |u| u.admin } do
